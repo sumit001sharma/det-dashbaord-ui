1e4e90fd5e289999392e114b9ced5c12be1f30c4
sumit001.ssharma <sumit001.ssharma@gmail.com>
2026-01-07 22:45:25 +0530

    added build folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'build',     //change from dist → build
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     open: true
